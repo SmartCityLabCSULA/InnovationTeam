43049894699251fe56af0f36441689622caf99b2
jay ess <mentoce@gmail.com>
2018-04-06 22:12:53 -0700

    Add data collection/plotting for UMSS team

--- python/pyplotdata/collectdata.py
+++ python/pyplotdata/collectdata.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import signal
 import time
 
@@ -8,12 +9,21 @@ import numpy as np
 import pandas as pd
 
 
-description = 'Utility to save data from serial to a CSV file'
+description  = 'Utility to save data from serial to a CSV file. Default port: '
+description += '/dev/ttyACMO, default baud rate: 115200'
+f_arg_help  = 'name for the CSV file. If no file name is passed then the '
+f_arg_help += 'default file name has the following format:'
+f_arg_help += 'Year-month-day-Hour-Minute-Second.csv'
 parser = argparse.ArgumentParser(description=description)
-parser.add_argument('-f', '--file', help='name for the CSV file')
-parser.add_argument('-p', '--plot', help='plot the data', action='store_true')
-parser.add_argument('--saveplot', 
+parser.add_argument('-f', '--file', help=f_arg_help, action='store', dest='filename')
+parser.add_argument('-p', '--plot', help='plot the data', 
+        action='store_true', dest='plot')
+parser.add_argument('--saveplot', action='store', dest='saveplot',
         help='saves plotted data, must give filename (used with --plot)')
+parser.add_argument('--port', help='serial port (default /dev/ttyACM0)',
+        default='/dev/ttyACM0', action='store', dest='port')
+parser.add_argument('-b', '--baud', help='baud rate (default 115200)', 
+        default=115200, action='store', dest='baud')
 
 args = parser.parse_args()
 
@@ -27,10 +37,9 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 interrupted = False
 
-
-print("Connecting to serial port...")
-ser = serial.Serial('/dev/ttyACM0', 115200)
-print("Connected to serial port")
+print("Connecting to serial port {}".format(args.port))
+ser = serial.Serial(args.port, args.baud)
+print("Connected to serial port {} at baud rate {}".format(args.port, args.baud))
 
 if args.plot:
     print("matplotlib setup stuff happening")
@@ -53,29 +62,36 @@ while not interrupted:
         data = ser.readline()
         data = data.decode("utf-8")
         data_split = data.split(",")
-        distance1 = data_split[0]
-        distance2 = data_split[1]
         distance1 = float(data_split[0])
         distance2 = float(data_split[1])
+        voltage3 = float(data_split[2])
+        voltage4 = float(data_split[3])
+        voltage5 = float(data_split[4])
+        # distance1 = float(data_split[0])
+        # distance2 = float(data_split[1])
         now = time.time()
         time_elapsed = now - start
-        temp_data = [time_elapsed, distance1, distance2]
+        temp_data = [time_elapsed, distance1, distance2, voltage3, voltage4, voltage5]
         data_list.append(temp_data)
         if args.plot:
             plt.show()
-            plt.scatter(time_elapsed, distance1, c='red')
-            plt.scatter(time_elapsed, distance2, c='blue')
+            plt.scatter(time_elapsed, distance1, c='red', s=5)
+            plt.scatter(time_elapsed, distance2, c='blue', s=5)
+            plt.scatter(time_elapsed, voltage3, c='green', s=5)
+            plt.scatter(time_elapsed, voltage4, c='yellow', s=5)
+            plt.scatter(time_elapsed, voltage5, c='black', s=5)
             plt.pause(pause)
     except:
         print("Couldn't get data")
 
 print('Data collection complete.')
-columns = ['Time', 'Distance 1', 'Distance 2'] 
-data_frame = pd.DataFrame(data_list, columns=columns)
-if args.file:
-    filename = '{}.csv'.format(args.file)
+data_frame = pd.DataFrame(data_list)
+if args.filename:
+    filename = '{}.csv'.format(args.filename)
 else:
-    filename = 'test.csv'
+    ts = time.time()
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
+    filename = '{}.csv'.format(timestamp)
 
 data_frame.to_csv(filename, index=False)
 print('Data written to {}'.format(filename))
