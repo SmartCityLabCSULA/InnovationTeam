2e0cac7ced72e54fdd220a24fcdd5ef809bacf48
jay ess <mentoce@gmail.com>
2018-04-08 16:26:41 -0700

    Finish first iteration of Schmitt trigger

--- include/simulation/schmitt_trigger.h
+++ include/simulation/schmitt_trigger.h
@@ -4,21 +4,47 @@
 
 namespace schmitt_trigger
 {
-// Simulates a Schmitt trigger (https://en.wikipedia.org/wiki/Schmitt_trigger).
+// Simulates a Schmitt trigger (https://en.wikipedia.org/wiki/Schmitt_trigger)
 // Converts a voltage to a 1 or 0 (high or low) based on the voltage 
 // thresholds.
 //
 // Parameters:
-// @voltage (double): the voltage reading
+// @voltage (double array): the voltage reading (needs to be the same size of 
+//      logic)
 // @thresh_low (double): the voltage value to be considered a 0
 // @thresh_high (double): the voltage value to be considered a 1
+// @logic (double array): this array is the output of the Schmitt trigger 
+//      (needs to be the same size as voltage)
+// @length (int): length of the voltage and logic arrays
 //
-// Returns:
-// @logic_val (int): 0 (low), 1 (high)
-//
-int schmitt_trigger(double voltage, double thresh_low, double thresh_high)
+void schmitt(double voltage[], double thresh_low, double thresh_high, 
+             double logic[], int length)
 {
-    
+    bool limit = false;
+    for(int i = 0; i < length; i++)
+    {
+        if (limit == false)
+        {
+            logic[i] = 0;
+        }
+        else 
+        {
+            logic[i] = 1;
+        }
+
+        if (voltage[i] <= thresh_low)
+        {
+            limit = false;
+            logic[i] = 0;
+        }
+        else if (voltage[i] >= thresh_high)
+        {
+            limit = true;
+            logic[i] = 1;
+        }
+    }
 }
+
+//int count_pulses(double wave
 } // namespace schmitt_trigger
 #endif // SIMULATION_SCHMITT_TRIGGER_H
