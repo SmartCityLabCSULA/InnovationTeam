e28abe635567560adbefa82614820729f146b798
jay ess <mentoce@gmail.com>
2018-04-08 19:42:53 -0700

    Changes reflect new namespacing

--- src/schmitt_sim.cpp
+++ src/schmitt_sim.cpp
@@ -2,16 +2,17 @@
 #include <iostream>
 
 #include "simulation/schmitt_trigger.h"
+#include "hw/schmitt_trigger.h"
 #include "hw/encoder.h"
 
 int main(int arcg, char *argv[])
 {
-    double run_time = 50.0; // seconds
+    double run_times = 50.0; // seconds
     double sample_rate = 0.0005; // seconds
-    int samples = int (run_time / sample_rate);
+    int samples = int (run_times / sample_rate);
     double sine_wave[samples];
     double frequency = 30;
-    double time[samples];
+    double times[samples];
     double t = 0.0;
 
     // Create the sine wave
@@ -19,7 +20,7 @@ int main(int arcg, char *argv[])
     {
         t = double (i);
         t *= sample_rate;
-        time[i] = t;
+        times[i] = t;
         sine_wave[i] = 0.5 * (1 + std::sin(2*M_PI*frequency*t));
         std::cout << sine_wave[i] << "," << t << std::endl;
     }
@@ -34,13 +35,13 @@ int main(int arcg, char *argv[])
     schmitt_trigger_sim::schmitt(sine_wave, thresh_low, thresh_high, pulses, samples);
     for (int i = 0; i < samples; i++)
     {
-        std::cout << time[i] << "," << pulses[i] << std::endl;
+        std::cout << times[i] << "," << pulses[i] << std::endl;
     }
 
     std::cout << std::endl << std::endl;
 
 
-    int ticks = schmitt_trigger_sim::count_pulses(pulses, samples);
+    int ticks = schmitt_trigger::count_pulses(pulses, samples);
 
     double circumference = 2*M_PI*23.17/2;
     int pulses_per_rev = 4;
